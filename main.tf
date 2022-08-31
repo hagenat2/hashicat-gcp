f92f6bfe9c764aadeec709eb926ee4a8d48cd95b
hagenat2 <nataliehagenow@gmail.com>
2022-08-31 18:16:48 +0000

    added the first label

--- main.tf
+++ main.tf
@@ -47,6 +47,7 @@ resource "google_compute_instance" "hashicat" {
   name         = "${var.prefix}-hashicat"
   zone         = "${var.region}-b"
   machine_type = var.machine_type
+  department   = "deveops"
 
   boot_disk {
     initialize_params {
